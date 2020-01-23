7e2b08d7357db1b97ab2b731f39034d2c6eea4e7
Keith Potempa <keithrpotempa@gmail.com>
2020-01-23 15:23:36 -0600

    added search bar and 'show all' button

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,6 +1,8 @@
-const getMovies = () => {
-    const movieURL = "http://localhost:8088/movies"
-    fetch(movieURL)
+const movieURL = "http://localhost:8088/movies"
+
+const getMovies = (URL) => {
+    movieContainer.innerHTML = "";
+    fetch(URL)
         .then(resp => resp.json())
         .then(movies => {
             renderMovies(movies);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,16 @@
-getMovies();
+// Removed this, as the buttons trigger it:
+// getMovies(movieURL);
 
+// Search Bar & Buttons
+const showallButton = document.querySelector("#showall-button");
+const searchButton = document.querySelector("#search-button");
+const searchBar = document.querySelector("#search-bar");
+
+searchButton.addEventListener("click", event => {
+    const search = searchBar.value;
+    getMovies(`http://localhost:8088/movies?q=${search}`);
+})
+
+showallButton.addEventListener("click", event => {
+    getMovies(movieURL);
+})
