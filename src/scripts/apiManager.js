5867007165b9a9d35eefaadcd41bf81bb34b716a
Keith Potempa <keithrpotempa@gmail.com>
2020-01-24 09:01:00 -0600

    external api working, but need a way to safely store key, it is not committed here

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,10 +1,28 @@
 const movieURL = "http://localhost:8088/movies"
+// TODO: Figure out how to keep apiKey elsewhere
+const apiKey = ""
 
 const getMovies = (URL) => {
     movieContainer.innerHTML = "";
     fetch(URL)
         .then(resp => resp.json())
         .then(movies => {
-            renderMovies(movies);
+            movies.forEach(movie => {
+                fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`)
+                    .then(response => response.json())
+                    .then(movieDetails => {
+                        const rated = movieDetails.Rated;
+                        const runtime = movieDetails.Runtime;
+                        const genre = movieDetails.Genre;
+                        const director = movieDetails.Director
+                        
+                        movie.rated = rated;
+                        movie.runtime = runtime;
+                        movie.genre = genre;
+                        movie.director = director;
+
+                        renderMovie(movie)
+                    })
+            })
         })
     }
--- src/scripts/movieManager.js
+++ src/scripts/movieManager.js
@@ -1,17 +1,18 @@
 const movieContainer = document.querySelector("#movieContainer");
 
-const renderMovies = (movies) => {
-    movies.forEach(movie => {
-        movieContainer.innerHTML += movieFactory(movie);
-    })
+const renderMovie = (movie) => {
+    movieContainer.innerHTML += movieFactory(movie);
 }
 
 const movieFactory = (movie) => {
     return `
     <article class="movie">
-        <p> <strong>Movie Title:</strong> ${movie.title} </p>
-        <p> <strong>Date:</strong> ${movie.releaseDate} </p>
-        <p> <strong>IMDB: </strong><a href="${movie.imdbLink}">Link</a> </p>
+        <p><strong>Movie Title:</strong> ${movie.title} </p>
+        <p><strong>Date:</strong> ${movie.releaseDate} </p>
+        <p><strong>Rating:</strong> ${movie.rated}</p>
+        <p><strong>Runtime:</strong> ${movie.runtime}</p>
+        <p><strong>Genre:</strong> ${movie.genre}</p>
+        <p><strong>Director:</strong> ${movie.director}</p>
     </article>
     `;
 }
